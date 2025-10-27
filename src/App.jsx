97833c6e644368ad4c770845b44da3e7b22d9473
leo-cristo <leocristo523@gmail.com>
2025-10-27 20:19:02 -0300

    erro impor highlights

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Colors from "./components/Colors";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
-import Highlights from "./components/highlights";
+import Highlights from "./components/Highlights";
 import Intro from "./components/Intro";
 import NavBar from "./components/NavBar";
 
