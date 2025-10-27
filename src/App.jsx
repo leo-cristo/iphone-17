aeda2a73c114b7e2dbc75289a52e68a90a18d04e
leo-cristo <leocristo523@gmail.com>
2025-10-27 20:14:26 -0300

    primeiro commit - Projeto clone iphon17

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Colors from "./components/Colors";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Highlights from "./components/highlights";
+import Intro from "./components/Intro";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+      <Hero/>
+      <Intro/>
+      <Highlights/>
+      <Colors/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Highlights.jsx
@@ -0,0 +1,102 @@
+function Highlights() {
+  const specs = [
+    {
+      value: "48MP",
+      label: "Principal",
+      text: "Sensor quad-pixel com foco automático",
+      color: "text-blue-600",
+    },
+    {
+      value: "12MP",
+      label: "Ultra Wide",
+      text: "Campo de visão 120° com modo noturno",
+      color: "text-orange-500",
+    },
+    {
+      value: "12M",
+      label: "Telefoto 5x",
+      text: "Zoom óptico de 5x com estabilização",
+      color: "text-blue-600",
+    },
+  ];
+
+  return (
+    <section className="bg-black py-20 px-6 " id="design">
+      <div className="max-w-7xl mx-auto">
+        <div className="text-center mb-16">
+          <h2 className="text-5xl font-bold mb-4">Design revolucionário</h2>
+          <p className="text-xl text-gray-400">
+            {" "}
+            Cada detalhe foi pensado para criar a melhor experiência
+          </p>
+        </div>
+        <div className="grid grid-cols-2 gap-8 mb-16">
+          <div className="bg-gray-900 rounded-3xl p-8">
+            <img
+              className="w-full rounded-2xl mb-4"
+              src="../public/img/titanium-design.jpg"
+              alt="iphone-titanium"
+            />
+            <h3 className="font-bold mb-2 text-3xl">Titânio Premium</h3>
+            <p className="text-gray-300">
+              Estrutura em titânio de grau aeroespacial. O smartphone mais forte
+              e leve.
+            </p>
+          </div>
+          <div className="bg-gray-900 rounded-3xl p-8">
+            <img
+              className="w-full rounded-2xl mb-4"
+              src="../public/img/ios-features.jpg"
+              alt="ios 2025"
+            />
+            <h3 className="font-bold mb-2 text-3xl">iOS 26</h3>
+            <p className="text-gray-300">
+              O sistema operacional mais avançado do mundo com IA integrada.
+            </p>
+          </div>
+        </div>
+
+        <div className="bg-gray-900 rounded-3xl p-12 mb-16" id="performance">
+          <h3 className="text-4xl font-bold mb-6 text-gradient">A18 Pro</h3>
+          <p className=" text-gray-300 mb-6">
+            O Chip mais poderoso em um smartphone
+          </p>
+          <img
+            className="w-full rounded-2xl mb-4"
+            src="../public/img/chip-a18-pro.jpg"
+            alt="cip a18"
+          />
+          <ul className="space-y-3 text-gray-300">
+            <li className="">CPU 20% mais rápida</li>
+            <li>GPU 25% mais eficiente</li>
+            <li>Neural Engine com 16 núclos</li>
+            <li>Ray tracing acelerado por hardware</li>
+          </ul>
+        </div>
+        <div>
+          <div id="camera" className="text-center">
+            <h3 className="text-4xl font-bold mb-10">
+              Sistema de câmera Pro avançado
+            </h3>
+            <div className="grid grid-cols-3  gap-6 mx-auto max-w-5xl">
+              {specs.map((spec, index) => (
+                <div
+                  key={index}
+                  className="bg-gray-900 rounded-2xl p-8 hover:bg-gray-800 transition-all duration-300 hover:scale-105 cursor-pointer"
+                >
+                  <p className={`text-4xl font-bold ${spec.color}`}>
+                    {spec.value}
+                  </p>
+                  <p className="text-xl font-semibold mb-2">{spec.label}</p>
+                  <p className="text-gray-400">{spec.text}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Highlights;
